66c5e88fcf771e9b085539e883bf54fd7ce67aed
Anusha <anusham952@gmail.com>
2025-07-17 15:03:08 +0530

    Handling autodropdown: AutoelectDropDown.spec.js

--- tests/AutoselectDropDown.spec.js
+++ tests/AutoselectDropDown.spec.js
@@ -1,4 +1,12 @@
 const{test, expect} = require('@playwright/test');
 test('Handling the Autodropdown', async({page})=>{
-    
+    await page.goto('https://www.amazon.in/');
+    await page.locator('//input[@id="twotabsearchtextbox"]').fill('cloths');
+
+    await page.waitForSelector('//div[contains(@id, "sac-suggestion")]');
+
+    const suggestElems=await page.locator('//div[contains(@id, "sac-suggestion")]//span');
+    await suggestElems.nth(2).click();
+
+    await page.waitForTimeout(5000);
 })
