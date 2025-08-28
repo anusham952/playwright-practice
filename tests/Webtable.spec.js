2bf405271e353aa78286fc592a836d48740a9d5d
Anusha <anusham952@gmail.com>
2025-08-28 13:16:22 +0530

    added a file

--- tests/Webtable.spec.js
+++ tests/Webtable.spec.js
@@ -42,6 +42,30 @@ async function multiSelect(rows, page, name){
 await multiSelect(rows,page,'Laptop');
 await multiSelect(rows,page,'Tablet');
 
+
+//how to extract data from the table
+
+// for(let i=0; i< await rows.count(); i++){
+//     const row=rows.nth(i);
+//     const tds= row.locator('td');
+
+//     for(let j=0; j< await tds.count()-1; j++){
+//         console.log(await tds.nth(j).textContent());
+//     }
+// }
+
+// read data from all the pages in a table
+
+const pages= await page.locator('.pagination');
+
+
+
+
+
+
+
+
+
 await page.waitForTimeout(5000);
 
 
